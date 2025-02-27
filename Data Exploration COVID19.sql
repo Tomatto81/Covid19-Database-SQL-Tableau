a4a340412f28b7355b6014cd847981ee3fad9ad1
Matto <66933672+Tomatto81@users.noreply.github.com>
2025-02-28 06:06:32 +0700

    Update Data Exploration COVID19.sql

--- Data Exploration COVID19.sql
+++ Data Exploration COVID19.sql
@@ -24,7 +24,7 @@ order by 1,2
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From [COVID-19]..CovidDeaths$
-Where location like '%Bhutan%'
+Where location like '%Indonesia%'
 and continent is not null 
 order by 1,2
 
@@ -33,14 +33,14 @@ order by 1,2
 -- Shows what percentage of population infected with Covid
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
-From [COVID-19]..CovidDeaths
+From [COVID-19]..CovidDeaths$
 order by 1,2
 
 
 -- Countries with Highest Infection Rate compared to Population
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From [COVID-19]..CovidDeaths
+From [COVID-19]..CovidDeaths$
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -48,7 +48,7 @@ order by PercentPopulationInfected desc
 -- Countries with Highest Death Count per Population
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From [COVID-19]..CovidDeaths
+From [COVID-19]..CovidDeaths$
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
@@ -60,7 +60,7 @@ order by TotalDeathCount desc
 -- Showing contintents with the highest death count per population
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From [COVID-19]..CovidDeaths
+From [COVID-19]..CovidDeaths$
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -70,7 +70,7 @@ order by TotalDeathCount desc
 -- GLOBAL NUMBERS
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From [COVID-19]..CovidDeaths
+From [COVID-19]..CovidDeaths$
 where continent is not null 
 order by 1,2
 
@@ -81,8 +81,8 @@ order by 1,2
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
-From [COVID-19]..CovidDeaths dea
-Join [COVID-19]..CovidVaccinations vac
+From [COVID-19]..CovidDeaths$ dea
+Join [COVID-19]..CovidVaccinations$ vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
@@ -96,8 +96,8 @@ as
 (
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
-From [COVID-19]..CovidDeaths dea
-Join [COVID-19]..CovidVaccinations vac
+From [COVID-19]..CovidDeaths$ dea
+Join [COVID-19]..CovidVaccinations$ vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
@@ -122,9 +122,9 @@ RollingPeopleVaccinated numeric
 
 Insert into #PercentPopulationVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
-From [COVID-19]..CovidDeaths dea
-Join [COVID-19]..CovidVaccinations vac
+, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From [COVID-19]..CovidDeaths$ dea
+Join [COVID-19]..CovidVaccinations$ vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 
@@ -136,7 +136,7 @@ From #PercentPopulationVaccinated
 
 -- Creating View to store data for later visualizations
 
-Create View PercentPopulationVaccinated as
+Alter View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 From [COVID-19]..CovidDeaths$ dea
